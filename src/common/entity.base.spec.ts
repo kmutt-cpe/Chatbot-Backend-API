371ad37e46e8ce1d7806538e38e95c2e22a94f0f
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2020-10-12 16:58:02 +0700

    Add entity test

---
+++ src/common/entity.base.spec.ts
@@ -0,0 +1,66 @@
+import { BaseEntity } from './entity.base';
+
+/**
+ * Temporary Entity for test the BaseEntity.
+ */
+class Entity extends BaseEntity {
+  private entityData1 = 'data1';
+  private entityData2 = 'data2';
+  private entityData3 = 'data3';
+}
+
+describe('BaseEntity', () => {
+  let entity: Entity;
+
+  beforeEach(async () => {
+    entity = new Entity();
+  });
+
+  describe('getData', () => {
+    it('should get all data inside entity', () => {
+      const expectedData = {
+        entityData1: 'data1',
+        entityData2: 'data2',
+        entityData3: 'data3',
+      };
+      expect(entity.getData()).toStrictEqual(expectedData);
+    });
+  });
+
+  describe('setDataValue', () => {
+    beforeEach(async () => {
+      entity.setDataValue('id', '1');
+      entity.setDataValue('entityData1', 'data4');
+    });
+    it(`should set data correctly`, () => {
+      const expectedData = {
+        entityData1: 'data4',
+        entityData2: 'data2',
+        entityData3: 'data3',
+      };
+      expect(entity.getData()).toStrictEqual(expectedData);
+    });
+    it(`shouldn't set id`, () => {
+      expect(entity.getData().id).toBeUndefined();
+    });
+  });
+
+  describe('setDataValues', () => {
+    beforeEach(async () => {
+      entity.setDataValues({
+        id: 1,
+        entityData1: 'data4',
+        entityData2: 'data5',
+        entityData3: 'data6',
+      });
+    });
+    it(`should set data correctly`, () => {
+      const expectedData = {
+        entityData1: 'data4',
+        entityData2: 'data5',
+        entityData3: 'data6',
+      };
+      expect(entity.getData()).toStrictEqual(expectedData);
+    });
+  });
+});
