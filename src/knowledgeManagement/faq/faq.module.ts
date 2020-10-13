1eb6d55c7a4d36c3560b042a26e513fca6e5ff09
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2020-10-13 13:39:58 +0700

    Implement FAQ module

--- src/knowledgeManagement/faq/faq.module.ts
+++ src/knowledgeManagement/faq/faq.module.ts
@@ -1,1 +1,10 @@
-// todo: Crate faq module
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FAQRepository } from './repositories/faq.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([FAQRepository])],
+  providers: [],
+  controllers: [],
+})
+export class FAQModule {}
