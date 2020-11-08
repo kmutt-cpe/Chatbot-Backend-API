d4c57d09647184e9b2159cf01bc63e529d8ad20f
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2020-11-08 22:13:36 +0700

    Remove questionMessageRepository import

--- src/chatbot/chatbot.module.ts
+++ src/chatbot/chatbot.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatbotController } from './chatbot.controller';
 import { ChatbotService } from './chatbot.service';
-import { QuestionMessageRepository } from './repositories/questionMessage.repository';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([QuestionMessageRepository]),
-  ],
   controllers: [ChatbotController],
   providers: [ChatbotService],
 })
