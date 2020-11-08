b5783b817a0be217ce034e5726bbc3c5715e1134
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2020-11-08 22:13:57 +0700

    Changing wrong name of chatbotService

--- src/chatbot/chatbot.controller.ts
+++ src/chatbot/chatbot.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { ChatbotModelService } from './chatbot.service';
+import { ChatbotService } from './chatbot.service';
 
 @Controller()
 export class ChatbotController {
-  constructor(private readonly chatbotModelService: ChatbotModelService) {}
+  constructor(private readonly chatbotService: ChatbotService) {}
 
   @Get()
   getHello(): string {
-    return this.chatbotModelService.getHello();
+    return this.chatbotService.getHello();
   }
 }
