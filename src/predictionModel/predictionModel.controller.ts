bb348b4ca3821982cbdf00a1a4558f5e282bf956
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2020-11-22 03:25:30 +0700

    Changing api name to follow rulebook

--- src/predictionModel/predictionModel.controller.ts
+++ src/predictionModel/predictionModel.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreatePredictTaskDto } from './dto/createPredictTask.dto';
 import { PredictionModelService } from './predictionModel.service';
 
-@Controller('predictionModel')
+@Controller('prediction-model')
 export class PredictionModelController {
   constructor(private readonly predictionModelService: PredictionModelService) {}
 
-  @Post('createPredictTask')
+  @Post('create-predict-task')
   createPredictTask(@Body() createPredictTaskDto: CreatePredictTaskDto): Promise<string> {
     return this.predictionModelService.create(createPredictTaskDto);
   }
