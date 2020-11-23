8912596670375a34716fdfa79729314132d7b94f
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2020-11-24 01:59:38 +0700

    Create categoryDto and subcategoryDto

---
+++ src/knowledgeManagement/dto/category.dto.ts
@@ -0,0 +1,15 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Category as CategoryInterface } from 'knowledgeManagement/interfaces/category.interface';
+import { SubcategoryDto } from './subcategory.dto';
+
+@ObjectType()
+export class CategoryDto implements CategoryInterface {
+  @Field()
+  id: string;
+
+  @Field()
+  category: string;
+
+  @Field()
+  subcategories: SubcategoryDto[];
+}
