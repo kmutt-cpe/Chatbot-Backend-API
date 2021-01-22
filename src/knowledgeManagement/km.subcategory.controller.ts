c09281a54c05b920b2896b726ad0ffa25fe8cf8b
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-01-22 19:14:59 +0700

    Implement subcategory

--- src/knowledgeManagement/km.subcategory.controller.ts
+++ src/knowledgeManagement/km.subcategory.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateSubcategoryDto } from './dto/subcategory.create.dto';
+import { SubcategoryDto } from './dto/subcategory.dto';
+import { UpdateSubcategoryDto } from './dto/subcategory.update.dto';
 import { SubcategoryService } from './km.subcategory.service';
 
 @Controller('km/subcategory')
@@ -6,7 +9,34 @@ export class SubcategoryController {
   constructor(private readonly subcategoryService: SubcategoryService) {}
 
   @Get()
-  getHello(): string {
-    return this.subcategoryService.getHello();
+  async getCategory(): Promise<SubcategoryDto[]> {
+    return (await this.subcategoryService.getAllSubcategory()).map((subcategory) =>
+      subcategory.getData()
+    );
+  }
+
+  @Get(':id')
+  async getCategoryById(@Param('id') id: string): Promise<SubcategoryDto> {
+    return (await this.subcategoryService.getSubcategoryById(id)).getData();
+  }
+
+  @Patch()
+  async updateCategory(
+    @Body('id') id: string,
+    @Body('update') updateCategoryDto: UpdateSubcategoryDto
+  ): Promise<SubcategoryDto> {
+    return (await this.subcategoryService.updateSubcategory(id, updateCategoryDto)).getData();
+  }
+
+  @Post()
+  async createCategory(
+    @Body() createSubcategoryDto: CreateSubcategoryDto
+  ): Promise<SubcategoryDto> {
+    return (await this.subcategoryService.createSubcategory(createSubcategoryDto)).getData();
+  }
+
+  @Delete()
+  async deleteCategory(@Body('id') id: string): Promise<SubcategoryDto> {
+    return (await this.subcategoryService.deleteSubcategoryById(id)).getData();
   }
 }
