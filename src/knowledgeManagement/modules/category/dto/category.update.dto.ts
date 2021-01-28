7c90c5a0d55a6169172c6236727cb295985e05b9
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-01-28 13:11:14 +0700

    Changing type of id in dto

--- src/knowledgeManagement/modules/category/dto/category.update.dto.ts
+++ src/knowledgeManagement/modules/category/dto/category.update.dto.ts
@@ -1,12 +1,14 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { IsString } from 'class-validator';
 import { Category as CategoryInterface } from 'knowledgeManagement/modules/category/category.interface';
 
 @ObjectType()
 export class UpdateCategoryDto implements CategoryInterface {
-  id: undefined;
+  @Field(() => ID)
+  @IsString()
+  id: string;
 
-  @Field()
+  @Field(() => String)
   @IsString()
   category: string;
 
