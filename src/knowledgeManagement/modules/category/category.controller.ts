bb65d0bc77f12346bc2fff64a9a189cfecdc27d1
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-03-05 02:47:47 +0700

    Add guard in CUD of category module

--- src/knowledgeManagement/modules/category/category.controller.ts
+++ src/knowledgeManagement/modules/category/category.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { CategoryDto } from './dto/category.dto';
 import { CreateCategoryDto } from './dto/category.create.dto';
 import { CategoryService } from './category.service';
 import { UpdateCategoryDto } from './dto/category.update.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('km/category')
 export class CategoryController {
@@ -19,16 +20,19 @@ export class CategoryController {
   }
 
   @Patch()
+  @UseGuards(JwtAuthGuard)
   async updateCategory(@Body('update') updateCategoryDto: UpdateCategoryDto): Promise<CategoryDto> {
     return await this.categoryService.updateCategory(updateCategoryDto);
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   async createCategory(@Body() createCategoryDto: CreateCategoryDto): Promise<CategoryDto> {
     return await this.categoryService.createCategory(createCategoryDto);
   }
 
   @Delete()
+  @UseGuards(JwtAuthGuard)
   async deleteCategory(@Body('id') id: string): Promise<CategoryDto> {
     return await this.categoryService.deleteCategoryById(id);
   }
