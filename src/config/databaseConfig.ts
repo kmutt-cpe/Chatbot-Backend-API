1ff16f942bb9560cc1b81e1d6a5fe3e3a285c575
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-04-03 15:29:39 +0700

    Fix bug in config

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -3,16 +3,18 @@ import { DatabaseConfig } from './interfaces/databaseConfig.interface';
 const resolveBoolean = (value) => (value === true || value === 'true' ? true : false);
 
 export default (): DatabaseConfig => {
-  return {
+  const dbConfig = {
     databaseConfig: {
       type: process.env.DB_TYPE,
       host: process.env.DB_HOST,
-      port: parseInt(process.env.PORT, 10),
+      port: parseInt(process.env.DB_PORT, 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: resolveBoolean(process.env.DB_SYNCHRONIZE),
+      retryDelay: 10000,
     },
   };
+  return dbConfig;
 };
