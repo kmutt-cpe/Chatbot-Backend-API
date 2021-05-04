fe8a2781b6547334033e40b2d8278396945a6a67
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-05-04 20:19:21 +0700

    Add nestjs-dialogflow to project

---
+++ src/chatbot/chatbot.dialogflow.provider.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import {
+  DialogFlowFulfillmentResponse,
+  DialogFlowIntent,
+  DialogFlowParam,
+  DialogFlowResponse,
+  OutputContexts,
+  QueryResult,
+} from 'nestjs-dialogflow';
+
+@Injectable()
+export class MyDialogFlowProvider {
+  @DialogFlowIntent('ตารางสอบซ้อน')
+  public async handleMyIntent1(
+    dialogFlowResponse: DialogFlowResponse
+  ): Promise<DialogFlowFulfillmentResponse> {
+    console.log('Intent1', dialogFlowResponse);
+    return {} as DialogFlowFulfillmentResponse;
+  }
+
+  @DialogFlowIntent('My:intent2')
+  public async handleMyIntent2(
+    dialogFlowResponse: DialogFlowResponse
+  ): Promise<DialogFlowFulfillmentResponse> {
+    console.log('Intent2', dialogFlowResponse);
+    return {} as DialogFlowFulfillmentResponse;
+  }
+
+  @DialogFlowIntent('My:intent3')
+  public async handleMyIntent3(
+    @DialogFlowParam('queryResult.outputContexts') outputContexts: OutputContexts
+  ): Promise<DialogFlowFulfillmentResponse> {
+    console.log('Intent3', outputContexts);
+    return {} as DialogFlowFulfillmentResponse;
+  }
+
+  @DialogFlowIntent('My:intent4')
+  public async handleMyIntent4(
+    @DialogFlowParam('queryResult') queryResult: QueryResult
+  ): Promise<DialogFlowFulfillmentResponse> {
+    console.log('Intent4', queryResult);
+    return {} as DialogFlowFulfillmentResponse;
+  }
+}
--- src/chatbot/chatbot.module.ts
+++ src/chatbot/chatbot.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DialogFlowModule } from 'nestjs-dialogflow';
 import { ChatbotController } from './chatbot.controller';
 import { ChatbotService } from './chatbot.service';
 import { FBMessageRepository } from './domain/fbMessage.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FBMessageRepository])],
+  imports: [
+    TypeOrmModule.forFeature([FBMessageRepository]),
+    DialogFlowModule.forRoot({
+      basePath: 'chatbot',
+      postPath: 'dialog-flow',
+    }),
+  ],
   controllers: [ChatbotController],
   providers: [ChatbotService],
 })
