dd369f3bba60ece8f4543437c05456e5324180b6
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-05-05 19:05:08 +0700

    Add throw error in subcategory

--- src/knowledgeManagement/modules/subcategory/subcategory.service.ts
+++ src/knowledgeManagement/modules/subcategory/subcategory.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateSubcategoryDto } from './dto/subcategory.create.dto';
 import { UpdateSubcategoryDto } from './dto/subcategory.update.dto';
 import { Subcategory } from './domain/subcategory.entity';
@@ -28,33 +28,39 @@ export class SubcategoryService {
 
   async getSubcategoryById(id: string): Promise<SubcategoryDto> {
     const subcategory: Subcategory = await this.subcategoryRepo.findById(id);
+    if (!subcategory) return null;
     const subcategoryDto: SubcategoryDto = { ...subcategory.getData(), category: undefined };
     const category: Category = await subcategory.category;
     subcategoryDto.category = category.getData();
-    // todo: Throw error 404 if not found subcategory
     return subcategoryDto;
   }
 
   async deleteSubcategoryById(id: string): Promise<SubcategoryDto | null> {
     let subcategory: Subcategory = await this.subcategoryRepo.findById(id);
-    if (!subcategory)
-      // todo: Throw error 404 if not found subcategory
-      return null;
+    if (!subcategory) throw new HttpException('Not found subcategory', HttpStatus.NOT_FOUND);
+    if ((await subcategory.faqs).length !== 0)
+      throw new HttpException(
+        'Cannot remove subcategory because there are faqs left in subcategory',
+        HttpStatus.BAD_REQUEST
+      );
     subcategory = await this.subcategoryRepo.softRemove(subcategory);
+    if (!subcategory)
+      throw new HttpException('Cannot remove subcategory', HttpStatus.NOT_IMPLEMENTED);
     const subcategoryDto: SubcategoryDto = { ...subcategory.getData(), category: undefined };
     const category: Category = await subcategory.category;
     subcategoryDto.category = category.getData();
-    // todo: Remove subcategory
-    return subcategory ? subcategoryDto : null;
+    return subcategoryDto;
   }
 
   async createSubcategory(createSubcategoryDto: CreateSubcategoryDto): Promise<SubcategoryDto> {
     let subcategory: Subcategory = this.subcategoryRepo.create();
     const category = await this.categoryRepo.findById(createSubcategoryDto.categoryId);
-    // todo: Throw error 404 if not found category
+    if (!category) throw new HttpException('Not found category', HttpStatus.NOT_FOUND);
     subcategory.setDataValues(createSubcategoryDto);
     subcategory.category = Promise.resolve(category);
     subcategory = await this.subcategoryRepo.save(subcategory);
+    if (!subcategory)
+      throw new HttpException('Cannot create subcategory', HttpStatus.NOT_IMPLEMENTED);
     const subcategoryDto: SubcategoryDto = { ...subcategory.getData(), category: undefined };
     subcategoryDto.category = category.getData();
     return subcategoryDto;
@@ -63,11 +69,13 @@ export class SubcategoryService {
   async updateSubcategory(updateSubcategoryDto: UpdateSubcategoryDto): Promise<SubcategoryDto> {
     let subcategory = await this.subcategoryRepo.findById(updateSubcategoryDto.id);
     const category = await this.categoryRepo.findById(updateSubcategoryDto.categoryId);
-    // todo: Throw error 404 if not found subcategory
-    // todo: Throw error 404 if not found category
+    if (!(subcategory || category))
+      throw new HttpException('Not found category or subcategory', HttpStatus.NOT_FOUND);
     subcategory.setDataValues(updateSubcategoryDto);
     subcategory.category = Promise.resolve(category);
     subcategory = await this.subcategoryRepo.save(subcategory);
+    if (!subcategory)
+      throw new HttpException('Cannot update subcategory', HttpStatus.NOT_IMPLEMENTED);
     const subcategoryDto: SubcategoryDto = { ...subcategory.getData(), category: undefined };
     subcategoryDto.category = category.getData();
     return subcategoryDto;
