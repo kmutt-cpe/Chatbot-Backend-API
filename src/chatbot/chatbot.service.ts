21894ba525f609863468cf61825d3a91919574cb
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-05-07 19:52:36 +0700

    Import prediction model

--- src/chatbot/chatbot.service.ts
+++ src/chatbot/chatbot.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PredictionModelService } from 'predictionModel/predictionModel.service';
 import { ChatMessage } from './domain/chatMessage.entity';
 import { ChatMessageRepository } from './domain/chatMessage.repository';
 
 @Injectable()
 export class ChatbotService {
-  constructor(private readonly chatMessageRepository: ChatMessageRepository) {}
+  constructor(
+    private readonly chatMessageRepository: ChatMessageRepository,
+    private readonly predictionModelService: PredictionModelService
+  ) {}
 
   saveChatMessage(message: string): ChatMessage {
     const chatMessage: ChatMessage = this.chatMessageRepository.create();
