cfafebf4c71196367350db949373ee4b7350d2e1
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-05-07 20:19:08 +0700

    Add collation in chatbotMessage

--- src/chatbot/domain/chatMessage.entity.ts
+++ src/chatbot/domain/chatMessage.entity.ts
@@ -5,33 +5,33 @@ import { FBMessageDto as ChatMessageDto } from 'chatbot/dto/fbMessage.dto';
 
 @Entity()
 export class ChatMessage extends BaseEntity implements ChatMessageInterface {
-  @Column()
+  @Column({ collation: 'utf8_general_ci' })
   message: string;
 
-  @Column()
+  @Column({ collation: 'utf8_general_ci' })
   /** Id of message from dialogflow */
   responseId: string;
 
-  @Column()
+  @Column({ collation: 'utf8_general_ci' })
   /** Message from client */
   queryText: string;
 
-  @Column({ type: 'json' })
+  @Column({ type: 'json', collation: 'utf8_general_ci' })
   parameters: string;
 
-  @Column()
+  @Column({ collation: 'utf8_general_ci' })
   /** Path of agent, dialogflow: { intent:{ name : agent }} */
   agent: string;
 
-  @Column()
+  @Column({ collation: 'utf8_general_ci' })
   /** Name of agent, dialogflow: { intent:{ displayName : agent }} */
   agentName: string;
 
-  @Column()
+  @Column({ collation: 'utf8_general_ci' })
   /** Source of chat e.g. Line, Facebook */
   source: string;
 
-  @Column()
+  @Column({ collation: 'utf8_general_ci' })
   /** Use for reply msg back */
   replyToken: string;
 
