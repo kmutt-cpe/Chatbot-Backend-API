f89d3d547424302dc49902248ecd157ce06c3bd8
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-05-07 19:47:29 +0700

    Add more column in chatMessage entity

--- src/chatbot/domain/chatMessage.entity.ts
+++ src/chatbot/domain/chatMessage.entity.ts
@@ -9,7 +9,31 @@ export class ChatMessage extends BaseEntity implements ChatMessageInterface {
   message: string;
 
   @Column()
+  /** Id of message from dialogflow */
   responseId: string;
+
+  @Column()
+  /** Message from client */
+  queryText: string;
+
+  @Column({ type: 'json' })
+  parameters: string;
+
+  @Column()
+  /** Path of agent, dialogflow: { intent:{ name : agent }} */
+  agent: string;
+
+  @Column()
+  /** Name of agent, dialogflow: { intent:{ displayName : agent }} */
+  agentName: string;
+
+  @Column()
+  /** Source of chat e.g. Line, Facebook */
+  source: string;
+
+  @Column()
+  /** Use for reply msg back */
+  replyToken: string;
 
   getData(): ChatMessageDto {
     return { id: this.id, message: this.message };
