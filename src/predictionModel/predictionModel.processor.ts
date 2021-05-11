3d334253c47334b2ee971466aaf98c90aacc5f91
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-05-11 23:38:13 +0700

    Connect prediction model to backend

--- src/predictionModel/predictionModel.processor.ts
+++ src/predictionModel/predictionModel.processor.ts
@@ -25,12 +25,17 @@ export class PredictionModelProcessor {
     await this.predictTaskRepo.save(predictTask);
 
     /** Sending to category model */
-    // todo: Use predicted category model
-    const predictedCategoryResponse = { category: 'test' };
+    const predictedCategoryResponse = await this.httpService
+      .post<{ category: string; accuracy: number }>(process.env.CATEGORY_MODEL_URL, {
+        inputQuestion,
+      })
+      .toPromise();
 
     /** After sending to model */
     predictTask.outputTimeCategory = new Date();
-    const { category } = predictedCategoryResponse;
+    const { category, accuracy } = predictedCategoryResponse.data;
+    predictTask.categoryAccuracy = accuracy;
+    predictTask.predictedCategory = category;
 
     /** Query questions */
     const questionsDto = await this.faqService.getAllFAQ();
@@ -59,21 +64,16 @@ export class PredictionModelProcessor {
     /** Sending to question model */
     // todo: Send to prediciton model
     const predictedResponse = await this.httpService
-      .get<{ intent: string; category: string; confidence: number }>(
-        'https://openapi.botnoi.ai/botnoi/ecommerce',
-        {
-          params: { keyword: inputQuestion },
-          headers: {
-            Authorization:
-              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjA2Nzk5ODcsImlkIjoiYzAyMWM3YmQtNWEyOS00MTE5LWE3OWMtZDI2NjMyNzQ3MDA4IiwiaXNzIjoiemczUTBJY3ZTakh2OWN4VG1HVXpzRWFDM09jbEprV0QiLCJuYW1lIjoi4LiZ4Li44LmJ4LiH4LiB4Lil4LmJ4LiyIiwicGljIjoiaHR0cHM6Ly9wcm9maWxlLmxpbmUtc2Nkbi5uZXQvMGhHdENRRW1rWUdGdC1LQTZ0ZHBSbkRFSnRGallKQmg0VEJob0RiMTR0Rld0VEdWcGZGMDllYmx3clFHZ0RUUXhZRWtZQVB3bDZGbTBIIn0.EiOY0bdsD7ekq6V4EB3IWfXysyofViBwYy3fkZECRLs',
-          },
-        }
-      )
+      .post<{ predictedQuestion: string; similarity: string }>(process.env.QUESTION_MODEL_URL, {
+        questions,
+        inputQuestion,
+      })
       .toPromise();
 
     /** After sending to question model */
     predictTask.outputTimeQuestion = new Date();
-    const predictedQuestion = predictedResponse.data && predictedResponse.data.intent;
+    const predictedQuestion = predictedResponse.data && predictedResponse.data.predictedQuestion;
+    const similarity = predictedResponse.data && predictedResponse.data.similarity;
     const questionEntity = questionFilter.find(
       (question) => question.question === predictedQuestion
     );
@@ -84,16 +84,17 @@ export class PredictionModelProcessor {
       predictTask.status = TaskStatus.FAILED_QUESTION;
       predictTask.predictedQuestionId = '';
       predictTask.predictedQuestion = predictedQuestion;
-      predictTask.predictedAnswer = 'โปรดลองใหม่อีกครั้ง';
+      predictTask.predictedAnswer = 'โปรดถามใหม่อีกครั้งค่ะ';
     } else if (!questionEntity) {
-      /** If cannot find question entity, ca */
+      /** If cannot find question entity */
       predictTask.status = TaskStatus.CANNOT_FIND_QUSTION;
       predictTask.predictedQuestionId = '';
       predictTask.predictedQuestion = predictedQuestion;
-      predictTask.predictedAnswer = questionEntity.answer;
+      predictTask.predictedAnswer = 'โปรดถามใหม่อีกครั้งค่ะ';
     } /* Else success */ else {
       predictTask.status = TaskStatus.SUCCESS_QUESTION;
       predictTask.predictedQuestionId = questionEntity.id;
+      predictTask.questionAccuracy = parseFloat(similarity);
       predictTask.predictedQuestion = predictedQuestion;
       predictTask.predictedAnswer = questionEntity.answer;
     }
